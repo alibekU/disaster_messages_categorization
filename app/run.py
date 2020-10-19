e3833569c68f88cf64ba45a1ce3696f19b633224
Alibek Utyubayev <uas91@hotmail.com>
2020-10-20 03:41:03 +0600

    feat: adds new plots

--- app/run.py
+++ app/run.py
@@ -7,10 +7,11 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+import plotly.graph_objs as Go
+import numpy as np
 import joblib
 from sqlalchemy import create_engine
-from support_functions import tokenize
+from support_functions import tokenize, cramers_v, create_heatmap_data
 
 
 app = Flask(__name__)
@@ -35,16 +36,69 @@ def index():
 
     # get the labels - Y dataframe - for visuals
     Y = df[df.columns[4:]]
+    # get the category names
+    category_names = Y.columns.to_list()
+    # sum the messages for each category and sort 
     sums_categories = Y.sum(axis=0).sort_values()
+    # get the names of the categories in the sorted order
     category_names_sorted = list(sums_categories.index)
-
+    
+    # create a matrix of correlations btw categories for heatmap graph
+    corr = create_heatmap_data(Y, cramers_v)
+    # create a dataframe from the matrix
+    corr_df = pd.DataFrame(data=corr, index=category_names, columns=category_names)
+   
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
-        # Genre distribution
+          # Correlation between classes
+        {
+            'data':[
+                Go.Heatmap(
+                    x = category_names,
+                    y = category_names,
+                    z= corr_df
+                )
+            ],
+            
+            'layout': {
+                'title': "Correlation between categories",
+                'height':800,
+                'yaxis': {
+                    'automargin': True
+                },
+                'xaxis': {
+                    'automargin': True
+                }
+            }
+        },
+        # Class distribution
         {
             'data': [
-                Bar(
+                Go.Bar(
+                    x=sums_categories,
+                    y=category_names_sorted,
+                    orientation='h',
+                    marker=dict(color = sums_categories)
+                )        
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'height':800,
+                'yaxis': {
+                    'title': "Categories",
+                    'automargin': True
+                },
+                'xaxis': {
+                    'title': "Count"
+                }
+            }
+        },
+      # Genre distribution
+        {
+            'data': [
+                Go.Bar(
                     x=genre_names,
                     y=genre_counts
                 )
@@ -57,28 +111,6 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
-                }
-            }
-        },
-        # Class distribution
-        {
-            'data': [
-                Bar(
-                    x=sums_categories,
-                    y=category_names_sorted,
-                    orientation='h',
-                    marker=dict(color = sums_categories)
-                )        
-            ],
-
-            'layout': {
-                'title': 'Distribution of Message Categories',
-                'yaxis': {
-                    'title': "Categories",
-                    'automargin': True
-                },
-                'xaxis': {
-                    'title': "Count"
                 }
             }
         }
